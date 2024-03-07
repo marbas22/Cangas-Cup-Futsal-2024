a47e776de8d8e9d4139bfaa95182e58fec6a654e
VicSDN <151613334+VicSDN@users.noreply.github.com>
2024-03-07 13:41:45 +0100

    Add first header test

---
+++ CangasCup2024/src/components/Countdown.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+
+function Countdown() {
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    function calculateTimeLeft() {
+        const startDate = new Date();
+        const endDate = new Date('June 14, 2024');
+        const difference = +endDate - +startDate;
+        let timeLeft = {
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0
+        };
+
+        if (difference > 0) {
+            timeLeft = {
+                days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60)
+            };
+        }
+
+        return timeLeft;
+    }
+
+    return (
+        <div>
+            {Object.keys(timeLeft).length === 0 ? (
+                <div>¡Ya ha comenzado!</div>
+            ) : (
+                <div>
+                    <div>Solo faltan...</div>
+                    <div>
+                        {timeLeft.days} días, {timeLeft.hours}:{timeLeft.minutes}:{timeLeft.seconds}
+                    </div>
+                    <div>14 de junio</div>
+                </div>
+            )}
+        </div>
+
+    );
+}
+
+export default Countdown;
+
